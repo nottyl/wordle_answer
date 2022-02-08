e11472c08c9c069c8a5df4c649c731953f35f0cc
not tyler <71718049+nottyl@users.noreply.github.com>
2022-02-08 14:37:30 +0800

    Version 1.0.1
    - The program now runs repeatedly until the user input is `-1`

--- main.c
+++ main.c
@@ -3,10 +3,16 @@
 #include "wordle.h"
 
 int main() {
-    int32_t issue;
+    int32_t issue = 0;
+    printf("WORDLE ANSWER BOT - INPUT -1 TO TERMINATE THE PROGRAM\n\n");
     printf("Please enter the Wordle number (e.g. Wordle 226): ");
     scanf("%d", &issue);
-    printf("The answer of Wordle %d is: ", issue);
-    wordle_answer(issue);
+    while(issue!=-1){
+        printf("The answer of Wordle %d is: ", issue);
+        wordle_answer(issue);
+        printf("\n");
+        printf("Please enter the Wordle number (e.g. Wordle 226): ");
+        scanf("%d", &issue);
+    }
     return 0;
 }
