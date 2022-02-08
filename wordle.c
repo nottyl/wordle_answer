bcde968a73f9409037b11b2c5b0535fadf4e5da6
not tyler <71718049+nottyl@users.noreply.github.com>
2022-02-08 19:46:25 +0800

    Version 1.0.1
    - Debugged
    - The array size has been corrected

--- wordle.c
+++ wordle.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void wordle_answer(int32_t a){
-    char words[1094][5] = {"cigar", "rebut", "sissy", "humph", "awake", "blush", "focal", "evade", "naval", "serve", "heath", "dwarf", "model", "karma", "stink", "grade",
+    char words[2004][5] = {"cigar", "rebut", "sissy", "humph", "awake", "blush", "focal", "evade", "naval", "serve", "heath", "dwarf", "model", "karma", "stink", "grade",
                     "quiet", "bench", "abate", "feign", "major", "death", "fresh", "crust", "stool", "colon", "abase", "marry", "react", "batty", "pride", "floss",
                     "helix", "croak", "staff", "paper", "unfed", "whelp", "trawl", "outdo", "adobe", "crazy", "sower", "repay", "digit", "crate", "cluck", "spike",
                     "mimic", "pound", "maxim", "linen", "unmet", "flesh", "booby", "forth", "first", "stand", "belly", "ivory", "seedy", "print", "yearn", "drain",
